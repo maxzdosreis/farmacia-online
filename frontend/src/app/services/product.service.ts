6ec2f12538617a19ac1a143b5234dd6ba13e8c96
Max Zimmermann dos Reis <maxzdosreis@gmail.com>
2026-02-11 23:05:40 -0300

    feat: create a ProductService using a paginated listing method

---
+++ frontend/src/app/services/product.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+
+  private apiUrl = 'http://127.0.0.1:8000/api/products';
+
+  constructor(private http: HttpClient) {}
+
+  getProducts(page: number = 1) {
+    return this.http.get<any>(`${this.apiUrl}?page=${page}`)
+  }
+  
+}
